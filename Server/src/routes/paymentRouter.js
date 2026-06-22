d2b520b989ba79c47294ef8d00f645c18bf0eeb8
MarvinNavarro01 <marvinnavarro15@gmail.com>
2026-06-22 13:36:33 +0800

    Fixing and Modifying PaymentController.

--- Server/src/routes/paymentRouter.js
+++ Server/src/routes/paymentRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { getPaymentStatus, handleWebhook, handleWebhookTEST, initiatePayment } from "../controller/paymentController.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 const paymentRouter = express.Router();
 
 paymentRouter.post('/initiate', express.json(), initiatePayment);
 paymentRouter.get('/status/:intentId', getPaymentStatus);
 paymentRouter.post('/webhook', express.raw({ type: 'application/json' }), handleWebhookTEST);
+paymentRouter.get('/payment-status/:intentId', getPaymentStatus);
 
 export default paymentRouter;
